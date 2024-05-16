f41d72b7f17c96a98118d70e83365e72f2649774
RealSilverMoon <qazhhhh@outlook.com>
2024-05-16 16:58:49 +0800

    Exclude tst again

--- src/main/java/com/silvermoon/boxplusplus/util/BoxRoutings.java
+++ src/main/java/com/silvermoon/boxplusplus/util/BoxRoutings.java
@@ -681,7 +681,7 @@ public class BoxRoutings {
             .collect(Collectors.toList());
         for (ItemStack machine : machineList) {
             if (getMetaTileEntity(machine) instanceof GT_MetaTileEntity_MultiBlockBase mte
-                && !mte.mName.startsWith("TST")) {
+                && !mte.mName.startsWith("TST") && !mte.mName.startsWith("name")) {
                 NetworkLoader.instance.sendToServer(
                     new MessageRouting(
                         new BoxRoutings(
