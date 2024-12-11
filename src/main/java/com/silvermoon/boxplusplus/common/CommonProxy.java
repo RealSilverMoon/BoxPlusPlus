781c9db5843c44ab3dfb1c433d204e9bc8e95468
luomolhx <2932965369@qq.com>
2024-12-11 21:41:03 +0800

    fix bug and move loader to proxy class

--- src/main/java/com/silvermoon/boxplusplus/common/CommonProxy.java
+++ src/main/java/com/silvermoon/boxplusplus/common/CommonProxy.java
@@ -1,7 +1,13 @@
 package com.silvermoon.boxplusplus.common;
 
+import static com.silvermoon.boxplusplus.boxplusplus.LOG;
+import static gregtech.api.enums.Mods.BartWorks;
+
+import net.minecraft.item.ItemStack;
+
 import com.silvermoon.boxplusplus.common.config.Config;
 import com.silvermoon.boxplusplus.common.loader.BlockRegister;
+import com.silvermoon.boxplusplus.common.loader.RecipeLoader;
 import com.silvermoon.boxplusplus.common.loader.TileEntitiesLoader;
 import com.silvermoon.boxplusplus.network.NetworkLoader;
 import com.silvermoon.boxplusplus.util.ResultModuleRequirement;
@@ -11,6 +17,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
+import gregtech.api.util.GTModHandler;
 
 public class CommonProxy {
 
@@ -34,5 +41,15 @@ public class CommonProxy {
     }
 
     // register server commands in this event handler (Remove if not needed)
-    public void serverStarting(FMLServerStartingEvent event) {}
+    public void serverStarting(FMLServerStartingEvent event) {
+        ItemStack modItem = GTModHandler.getModItem(BartWorks.ID, "gt.bwMetaGeneratedItem0", 64, 3);
+        if (modItem != null) {
+            LOG.info(
+                "{} is already registered",
+                modItem.getItem()
+                    .getUnlocalizedName());
+        } else {
+            new RecipeLoader().run();
+        }
+    }
 }
